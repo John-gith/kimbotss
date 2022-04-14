ab732e88e9da060b2fc7b8b4df3d037441aa1285
John-gith <100775197+John-gith@users.noreply.github.com>
2022-04-14 21:51:02 +0200

    Update configTwit.js

--- configTwit.js
+++ configTwit.js
@@ -1,8 +1,8 @@
 module.exports = {
-    consumer_key: 'cz6cyyipy767uywOLM0Qz9EoA',
-    consumer_secret: 'yfgPRXMDfr8LqQ4AL05eTuh5ENzJaSjMWUtXbLIn4FAGCNaXOn',
-    access_token: '1506730349870600198-f9nohjgwapnsk17uFLjNcc6Nx8Uh0E',
-    access_token_secret: 'NPFF2vMKeVgxUD9v2zkS54H8OexA2PjR7wdEDFs5uvQEq',
+    consumer_key: 'ZbZfADuTmcXNJcHNheS8NjnUB',
+    consumer_secret: 'DRb7vuUkWwo6QioT9PWF4bIJe8BWXGzYT9cryiRBY2xEVlhy2f',
+    access_token: '1506730349870600198-1WaAsY50JwRlsQXIF0znHhxiPbCBu6',
+    access_token_secret: 'MeS8h88XiKLPLhvB35a0DiGMzH4kRzLLFOcI5F2V3hJYw',
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     //strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
